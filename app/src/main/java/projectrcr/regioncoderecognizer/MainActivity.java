f2d402b6d46887f8242c2ac65b09338c193c5835
j4mm1n <d.ter.maat@gmail.com>
2015-11-20 23:00:04 +0100

    Initial design and layout

---
+++ app/src/main/java/projectrcr/regioncoderecognizer/MainActivity.java
@@ -0,0 +1,21 @@
+package projectrcr.regioncoderecognizer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+List<Integer> List_Display = new ArrayList<>();
+
+
+
+}
